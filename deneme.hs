5f95fc28b16721705fd1a71c7e9078e162717807
cyazar <cmyzr11@gmail.com>
2019-10-23 21:28:02 +0300

    asdf

--- deneme.hs
+++ deneme.hs
@@ -1,9 +1,12 @@
 myFirstFunction :: String -> String
 myFirstFunction input = "Hello World My Name is : sdfg asdf" ++ input
+myFirstIf a = if a <= 2
+  then a + 2
+  else a - 2
 
 main :: IO ()
 main = do
   putStrLn "Enter Your Name!"
   name <- getLine
-  let message = myFirstFunction name
+  let message = "You Entered : " ++ show name
   putStrLn message
